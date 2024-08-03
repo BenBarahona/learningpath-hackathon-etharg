bdeb6216b0cbf2949425a5dc25227521bb059d9f
BenBarahona <benjamin@helloiconic.com>
2024-08-03 10:45:51 -0300

    Added zupass admin auth

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,7 @@ import { useMemo, useCallback } from 'react';
 import { useConnect, Connector } from "wagmi";
 import { Box, Button, Container, Heading, Stack, Text,  Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react';
 import { ConnectOption} from '@/src/components/connectors'
+import { zuAuthPopup } from '@pcd/zuauth'
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -25,6 +26,53 @@ export default function Home() {
     [connect, onClose]
   );
 
+  //TODO: Agarrar el eventID, eventName de Firebase
+  //TODO: watermark agarrar del adress conectado
+  const watermark = "0xF4FFaa5bF781903e7187Db65Ee9Bd82153b3F7C1"
+  const eventId = "5b0d5213-e656-55cd-967f-88c7b1d6e15f";
+  const eventName = "Learning Path"
+  
+  const configs:any = [
+    {
+      "pcdType": "eddsa-ticket-pcd",
+      "publicKey": [
+        "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
+        "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
+      ],
+      eventId,
+      eventName
+    }
+  ]
+
+  const onOpenAdmin = async () => {
+    console.log('use zupass')
+    const result = await zuAuthPopup({
+      fieldsToReveal: {
+          revealTicketId: true,
+          },
+          watermark,
+          config: configs
+        })
+        console.log(watermark);
+  
+        console.log("Zupass Result: ", result)
+        if(result.type === "pcd") {
+            try {
+              console.log("PCD", result.pcdStr)
+              // const jsonPCD = JSON.parse(result.pcdStr)
+              // const pcd = await ZKEdDSAEventTicketPCDPackage.deserialize(jsonPCD.pcd)
+              // const proof = generateWitness(pcd)
+              // const encoder = ethers.AbiCoder.defaultAbiCoder();
+              // const data = encoder.encode(
+              //     ["uint256[2]", "uint256[2][2]", "uint256[2]", "uint256[38]"],
+              //     [proof._pA, proof._pB, proof._pC, proof._pubSignals],
+              // ) as `0x${string}`;
+            } catch (e) {
+              console.log("Failed", e)
+            }
+        }
+  }
+
 
   return (
     <Container maxW="md" centerContent py={10}>
@@ -43,7 +91,7 @@ export default function Home() {
         Bienvenido a Learning Path. Por favor, selecciona una opción para continuar.
       </Text>
       <Stack spacing={4}>
-        <Button colorScheme="teal" size="lg">
+        <Button colorScheme="teal" size="lg" onClick={onOpenAdmin}>
           Login como admin
         </Button>
         <Button variant="outline" size="lg" onClick={onOpen}>
