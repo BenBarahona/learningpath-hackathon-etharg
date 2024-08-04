8c92946503f2e5618c673588636b9f9120fe1610
Denilson Barahona <denilsonbarahona@Denilsons-MacBook-Air.local>
2024-08-04 09:06:00 -0300

    for token path

--- frontend/app/attempt/page.tsx
+++ frontend/app/attempt/page.tsx
@@ -1,34 +1,80 @@
 'use client'
 
-import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-
+import { useState, useMemo } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useWriteContract, useWaitForTransactionReceipt, useAccount } from "wagmi";
 import { Box, Text, Button } from '@chakra-ui/react';
 import { getQuestionsChallenges } from '@/src/services'
 import { useQuery } from '@tanstack/react-query'
 import { QuestionCard } from '@/src/components/questionCard'
+import { challengeAbi, challengeAddress} from '@/src/contract-abi/challenge'
 
 const AttemptPage = () => {
     
     const searchParams = useSearchParams()
-
-    const query = useQuery({ queryKey: ['questions_challenge', searchParams.get('challenge')], queryFn: () => getQuestionsChallenges(searchParams.get('challenge') as string) })
+    const router = useRouter();
+    const { address } = useAccount()
+    const query = useQuery({ queryKey: ['questions_challenge', searchParams.get('challenge')], queryFn: () => getQuestionsChallenges(searchParams.get('challenge') as string), select(data) {
+      return data as any
+    }, })
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedOption, setSelectedOption] = useState('');
+    const [correct, setCorrect] = useState(0);
+    const [finish, setFinish] = useState(false);
 
-      const handleSelectOption = (value: string) => {
-        setSelectedOption(value);
-      };
+    const { data: hash, error, isPending, writeContract } = useWriteContract();
+  
+    const { isLoading } = useWaitForTransactionReceipt({
+      hash,
+    });
 
-      const handleNextQuestion = () => {
-        if (currentQuestionIndex < Number(query.data?.length)- 1) {
-          setCurrentQuestionIndex(currentQuestionIndex + 1);
-          setSelectedOption('');
-        } else {
-          alert('Quiz completed!');
-        }
-      };
+    const isWriting = useMemo(() => {
+      return isPending || isLoading;
+    }, [isPending, isLoading]); 
+
+    const handleSelectOption = (value: string) => {
+      setSelectedOption(value);
+    };
+
+    const handleNextQuestion = () => {
+      if (query.data?.[currentQuestionIndex]?.options?.[selectedOption]?.isCorrect) {
+        setCorrect((prev)=>prev+1)
+      }
+      if (currentQuestionIndex < Number(query.data?.length)- 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setSelectedOption('');
+      } else {
+        handleSubmitQuest()
+        alert(`Quiz completed!, you answered, ${correct} questions correctly`);
+        setFinish(true)
+      }
+    };
+
+    const handleSubmitQuest = () => {
+      console.log(
+        {
+          abi: challengeAbi,
+          address: challengeAddress as `0x${string}`,
+          functionName: 'userCompletedChallenge',
+          args: [
+            address,
+            searchParams.get('challenge'),
+            correct
+          ],
+        }, 'userCompletedChallenge'
+      )
+      writeContract({
+        abi: challengeAbi,
+        address: challengeAddress as `0x${string}`,
+        functionName: 'userCompletedChallenge',
+        args: [
+          address,
+          searchParams.get('challenge'),
+          correct
+        ],
+      })
+    }
 
     return (
       <Box p="8" bg="gray.100" minH="100vh">
@@ -39,10 +85,16 @@ const AttemptPage = () => {
         question={query.data?.[currentQuestionIndex]}
         onSelectOption={handleSelectOption}
         selectedOption={selectedOption}
+        setCorrect={setCorrect}
       />
-      <Button onClick={handleNextQuestion} colorScheme="teal" mt="4">
+      {finish
+        ?<Button onClick={()=> router.push('/action')} isLoading={isWriting} colorScheme="teal" mt="4">
+          Regresar al home
+        </Button>
+        : <Button onClick={handleNextQuestion} isLoading={isWriting} colorScheme="teal" mt="4">
         Continuar
       </Button>
+      }
     </Box>
     );
   };
