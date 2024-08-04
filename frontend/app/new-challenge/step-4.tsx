a5194555d8e4028ae58c8d7807e6fd33e4821b29
Denilson Barahona <denilsonbarahona@Denilsons-MacBook-Air.local>
2024-08-03 23:13:23 -0300

    admin path finish

--- frontend/app/new-challenge/step-4.tsx
+++ frontend/app/new-challenge/step-4.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Box, Button, Flex, FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
 
-export const Step4 = () => {
+export const Step4 = ({
+    questionLength,
+    reward,
+    setReward,
+    submitChallenge
+}: any) => {
 
   return (
     <Box padding="6" boxShadow="lg" bg="white">
       <Text fontSize="xl" fontWeight="bold" marginBottom="4">4. Setup Total Challenge Reward</Text>
       <FormControl id="total-challenge-reward" isRequired>
         <FormLabel>Total Challenge Reward </FormLabel>
-        <Input placeholder="0 $PATH" defaultValue={0} type="number" />
+        <Input placeholder="0 $PATH" defaultValue={0} type="number" onChange={(event) => setReward(event.target.value)}/>
       </FormControl>
       <Text fontSize="sm" color="gray.500" marginTop="2">
         Important: The total reward for the challenge will be split evenly between all questions added,
         with players receiving the corresponding amount of $PATH depending on how many correct answers they have.
       </Text>
       <Box marginTop="4">
-        <Text>Total Reward: 0 $PATH</Text>
-        <Text>Amount of Questions: 2 questions</Text>
-        <Text>Reward per Question: 0 $PATH</Text>
+        <Text>Total Reward: {reward} $PATH</Text>
+        <Text>Amount of Questions: {questionLength} questions</Text>
+        <Text>Reward per Question: {reward/questionLength} $PATH</Text>
       </Box>
       <Flex justifyContent="flex-end" marginTop="6">
-        <Button colorScheme="orange" disabled>
+        <Button colorScheme="orange" disabled onClick={submitChallenge}>
           Continue
         </Button>
       </Flex>
