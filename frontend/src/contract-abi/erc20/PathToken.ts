fd39278ad9934a2ce2225df4bae8b00c7a629b2c
BenBarahona <benjamin@helloiconic.com>
2024-08-04 09:17:50 -0300

    Added PathToken abi

---
+++ frontend/src/contract-abi/erc20/PathToken.ts
@@ -0,0 +1,140 @@
+const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_pathToken",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_poolFirebaseId",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_tokenRequired",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_prizePerUser",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_tokenToBurn",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "Received",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "admin",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        }
+      ],
+      "name": "canClaimPrize",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "canClaim",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        }
+      ],
+      "name": "claimPrize",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "hasClaimed",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "pathToken",
+      "outputs": [
+        {
+          "internalType": "contract PathToken",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    }
+  ]
